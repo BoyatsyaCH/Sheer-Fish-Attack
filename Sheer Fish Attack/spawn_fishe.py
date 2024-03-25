cd7b25bef86e3927a4f8e08a4952272a5461cb25
BoyatsyaCH <87842162+BoyatsyaCH@users.noreply.github.com>
2024-03-25 15:03:37 -0300

    Update spawn_fishe.py
    Added fishe grouped collections

--- Sheer Fish Attack/spawn_fishe.py
+++ Sheer Fish Attack/spawn_fishe.py
@@ -1,24 +1,52 @@
 import bge
+from random import randint
 from random import uniform
-
 
 def main():
 
     cont = bge.logic.getCurrentController()
+    scene = bge.logic.getCurrentScene()
     own = cont.owner
 
     fish_listen = cont.sensors["spawnFishRelay"]
+    fish_counter = cont.sensors["Fish-Kill"]
+
+    round = cont.actuators['Round']
     fisheSpawn = cont.actuators["spawnFishe"]
+    fisheRound2 = cont.actuators["FishRound2"]
+    fisheRound3 = cont.actuators["FishRound3"]
+
+    Player = scene.objects["Player_Armature"]
+
+    if fish_counter.positive:
+        round.value = str(int(round.value) + 1)
+
 
     if fish_listen.positive:
-        minX = own.position.x - 3
-        maxX = own.position.x + 3
-        own.position.x = uniform(minX, maxX)
-        minY = own.position.y - 3
-        maxY = own.position.y + 3
-        own.position.y = uniform(minY, maxY)
-        own.position.z = 0
-        cont.activate(fisheSpawn)
+        choice = randint(0, 1)
+        if int(round.value) == 0:
+            Player["Round"] = 0
+            minX = own.position.x - 5
+            maxX = own.position.x + 5
+            own.position.x = uniform(minX, maxX)
+            minY = own.position.y - 5
+            maxY = own.position.y + 5
+            own.position.y = uniform(minY, maxY)
+            own.position.z = 0
+            cont.activate(fisheSpawn)
+            
+
+        if int(round.value) > 0 and choice == 0:
+            print("Fishe round 2")
+            Player["Kills"] = 0
+            Player["Round"] = 2
+            cont.activate(fisheRound2)
+
+        if int(round.value) > 0 and choice == 1:
+            print("Fishe round 3")
+            Player["Kills"] = 0
+            Player["Round"] = 3
+            cont.activate(fisheRound3)
     else:
         pass
 
