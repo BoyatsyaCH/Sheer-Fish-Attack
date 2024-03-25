33d778a47cbe39fcdff4a22370786baf4465da0b
BoyatsyaCH <87842162+BoyatsyaCH@users.noreply.github.com>
2024-03-25 14:45:51 -0300

    Update hand_equip.py
    Added shooting animation listen and action

--- Sheer Fish Attack/hand_equip.py
+++ Sheer Fish Attack/hand_equip.py
@@ -8,12 +8,14 @@ def main():
 
     equip_grenade = cont.sensors["Equip_Grenade"]
     equip_gun = cont.sensors["Equip_Gun"]
+    bullet_listen = cont.sensors["Bullet_Listen"]
     player_death = cont.sensors["Player_Death"]
 
     relay = cont.actuators["Relay"]
 
     grenade_anim = cont.actuators["Equip_Grenade"]
     gun_anim = cont.actuators["Equip_Gun"]
+    shot_anim = cont.actuators["Gun_Shot"]
     death_anim = cont.actuators["Death_Animation"]
 
     #Gun = scene.objects["Gun"].meshes[0]
@@ -28,6 +30,9 @@ def main():
         relay.subject = "Gun_Mesh"
         cont.activate(relay)
         cont.activate(gun_anim)
+
+    if bullet_listen.positive:
+        cont.activate(shot_anim)
 
     if player_death.positive:
         cont.activate(death_anim)
